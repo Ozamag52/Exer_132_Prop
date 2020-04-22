5b9d7f58ec587d05001a0f0beeece4988761a804
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-22 15:03:11 -0300

    Versao 0

---
+++ Exerc_132_Prop/Entities/Class/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_132_Prop.Entities.Class
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+        public string PriceTag()
+        {
+            return Name + " $ " + Price;
+        }
+    }
+}
---
+++ Exerc_132_Prop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerc_132_Prop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
